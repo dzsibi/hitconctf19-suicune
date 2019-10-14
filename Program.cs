8a682ef9c32545c2372535a04f5fc610930e39ce
Bálint József Jánvári <4534880+dzsibi@users.noreply.github.com>
2019-10-14 15:05:01 +0200

    Add write-up and code

---
+++ Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pcg;
+using SimpleBase;
+
+namespace Suicune
+{
+    /// <summary>
+    /// Brute force the key used to encrypt the flag.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Pre-calculated step counts for
+        /// reversing reverse sorted postfixes
+        /// </summary>
+        static readonly ulong[] STEP_COUNTS = new ulong[] {
+            1,
+            1,
+            2,
+            6,
+            24,
+            120,
+            720,
+            5040,
+            40320,
+            362880,
+            3628800,
+            39916800,
+            479001600UL,
+            6227020800UL,
+            87178291200UL,
+            1307674368000UL,
+            20922789888000UL,
+            355687428096000UL,
+            6402373705728000UL,
+            121645100408832000UL,
+            2432902008176640000UL
+        };
+
+        /// <summary>
+        /// Implements one step of the
+        /// original sorting algorithm.
+        /// </summary>
+        /// <param name="input">Input</param>
+        static bool SortStep(byte[] input)
+        {
+            // Start with the last two indices
+            int firstSwapIndex = input.Length - 2;
+            int secondSwapIndex = input.Length - 1;
+
+            // Pick first index
+            while (input[firstSwapIndex] > input[firstSwapIndex + 1])
+            {
+                if (firstSwapIndex == 0)
+                {
+                    // Sorted
+                    return true;
+                }
+                else
+                {
+                    --firstSwapIndex;
+                }
+            }
+
+            // Pick second index
+            while (input[secondSwapIndex] < input[firstSwapIndex])
+            {
+                --secondSwapIndex;
+            }
+
+            // Swap
+            var tmp = input[firstSwapIndex];
+            input[firstSwapIndex] = input[secondSwapIndex];
+            input[secondSwapIndex] = tmp;
+
+            // Reverese
+            Array.Reverse(input, firstSwapIndex + 1, input.Length - firstSwapIndex - 1);
+
+            // Not sorted
+            return false;
+        }
+
+        /// <summary>
+        /// Accelerated version of the original algorithm
+        /// that uses step estimation to determine where
+        /// the sorting should stop.
+        /// </summary>
+        /// <param name="input">Input</param>
+        /// <param name="limit">Limit</param>
+        static void FastShittySort(byte[] input, ulong limit)
+        {
+            while (limit > 0)
+            {
+                // Count reverse sorted bytes
+                // at the end of the input data
+                int index = input.Length - 2;
+                while (index > 0 && input[index] < input[index + 1])
+                {
+                    index--;
+                }
+                int length = input.Length - index - 1;
+
+                // If we have such a section, just reverse it and decrement
+                // the limit by the pre-calculated step count
+                if (length > 2 && length < STEP_COUNTS.Length && limit >= STEP_COUNTS[length] - 1)
+                {
+                    // Reverse 
+                    Array.Reverse(input, index + 1, length);
+
+                    // Decrement limit
+                    limit -= STEP_COUNTS[length] - 1;
+                }
+                else
+                {
+                    // Perform one standard sorting step
+                    if (SortStep(input))
+                    {
+                        return;
+                    }
+
+                    // Decrement limit
+                    --limit;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generate the XOR key for a specific PRNG seed.
+        /// </summary>
+        /// <param name="seed">Seed</param>
+        /// <param name="outputLength">Output length</param>
+        /// <returns>XOR key</returns>
+        static byte[] GenerateXorKey(ulong seed, int outputLength)
+        {
+            var buffer = new byte[256];
+            var key = new byte[outputLength];
+            var component = new byte[outputLength];
+
+            // Initialize random generator
+            var prng = new Pcg32(seed, 0);
+
+            // Each key is generated over 16 iterations
+            for (int i = 0; i < 16; ++i)
+            {
+                // Generate base array 
+                for (int j = 0; j < buffer.Length; ++j)
+                {
+                    buffer[j] = (byte)j;
+                }
+
+                // Shuffle array
+                for (int j = buffer.Length - 1; j >= 1; --j)
+                {
+                    var index = prng.GenerateNext((uint)j + 1);
+                    var old = buffer[index];
+                    buffer[index] = buffer[j];
+                    buffer[j] = old;
+                }
+
+                // We only need the first N elements
+                Array.Copy(buffer, 0, component, 0, component.Length);
+
+                // Generate limit
+                var part1 = prng.GenerateNext();
+                var part2 = prng.GenerateNext();
+                ulong limit = ((ulong)part2 << 32) | part1;
+
+                // Sort
+                FastShittySort(component, limit);
+
+                // XOR with current candidate
+                for (int j = 0; j < key.Length; ++j)
+                {
+                    key[j] ^= component[j];
+                }
+
+                // Reverse candidate
+                Array.Reverse(key);
+            }
+
+            // Return key
+            return key;
+        }
+
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                // Process input
+                var prefixBytes = Encoding.ASCII.GetBytes(args[0]);
+                var knownOutputBytes = Base16.Decode(args[1]).ToArray();
+
+                // Determine XOR key to look for
+                var prefixKey = prefixBytes.Select((b, i) => (byte)(b ^ knownOutputBytes[i])).ToArray();
+
+                // Iterate over all possible keys              
+                Parallel.For(0, UInt16.MaxValue + 1, (seed, state) =>
+                {
+                    // Generate key
+                    var xorKey = GenerateXorKey((ulong)seed, knownOutputBytes.Length);
+
+                    // Check if we got it right
+                    if (xorKey.Take(prefixKey.Length).SequenceEqual(prefixKey))
+                    {
+                        // Print seed
+                        Console.WriteLine($"Seed: {seed:X8}");
+
+                        // Decode flag
+                        var decodedBytes = knownOutputBytes.Select((b, i) => (byte)(b ^ xorKey[i])).ToArray();
+
+                        // Print flag
+                        var decodedString = Encoding.ASCII.GetString(decodedBytes);
+                        Console.WriteLine($"Flag: {decodedString}");
+
+                        // Success
+                        state.Stop();
+                    }
+                });
+            }
+            else
+            {
+                Console.WriteLine("Usage: Suicune <known prefix> <output>");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# hitconctf19-suicune
-Write-up on the Suicune challenge of HITCON CTF 2019
+# Apparently it's not efficient enough
+
+## Introduction
+
+This is a write-up on the HITCON CTF 2019 challenge  `Suicune`. We were provided with a x86_64 ELF executable that implemented an encryption algorithm of some sort:
+
+```
+dzsibi@OUROBOROS:~$ ./suicune
+Usage: ./suicune <flag> <key>
+dzsibi@OUROBOROS:~$ ./suicune test 128
+6edb96c1
+```
+
+The key was a simple integer, while the output was in hexadecimal format. Execution time increased exponentially as the plaintext value got bigger. A file with the expected output of the algorithm when it was applied to the real flag was also supplied. Cursory testing revealed a few things:
+
+* Output length matches input length
+* Change a bit in the input, it changes in the output
+* If you add or remove an input character, other parts of the output also change
+* With input sizes over 14-16 characters it seems to run indefinitely without printing any output
+
+Based on this, we concluded:
+
+* A keystream is generated based on the key and it is XOR-ed with the input
+* The keystream depends on the input length and the key
+
+We also knew the format of the flag: `hitcon{flag}`, with an overall length of 49 characters based on the supplied output.
+
+## The executable
+
+Looking at the executable it is immediately apparent that it was compiled using [Crystal](https://crystal-lang.org/), a Ruby-like language with an LLVM based compiler. Since debug symbols were present, I could immediately identify the `__crystal_main` function which was the entry point of the actual application - and which seemed to contain basically all user code, and even some framework code inlined by the compiler. After careful analysis, I was able to reverse-engineer the algorithm:
+
+1. Parse the key as an integer and truncate it to 16 bits
+2. Read the input into a byte array (the current state)
+3. Initialize a PCG32 based PRNG using the truncated key
+4. Perform 16 rounds of the same encryption algorithm:
+   1. Fill a 256 element array with the numbers from 0 to 255
+   2. Shuffle the array using Crystal's array#shuffle and the PRNG
+   3. Generate a 64 bit random number using the PRNG
+   4. Truncate the shuffled array to the input size
+   5. Sort the array
+   6. XOR the elements of the array with the input
+   7. Reverse the entire array
+5. After the rounds are done, print the elements as a hexadecimal string
+
+*Sidenote: At first, I did not notice the key truncation at all, but it was called out by one of my teammates well in the middle of the next phase.*
+
+So what was taking so long? Well, it turns out that the sorting algorithm is not exactly efficient. We can fix that!
+
+## Reimplementation
+
+There are only 2^16 possible keys (since the input is truncated), and the only real barrier to a brute force attack on the executable is the slow sorting algorithm - we just need to reimplement the algorithm using a faster one. Since we know the prefix of the flag, we can look for a match. After doing so, I was able to reverse example outputs, but not the actual flag output.
+
+What was going on? I kind of ignored the 64 bit number that was generated right in the middle, since it was a huge number that was decremented one-by-one at each iteration of the sorting algorithm, and it reaching zero was used as one of the (never used) exit conditions of the loop. Surely that would never happen. Oh wait.
+
+If the algorithm actually took more steps than the generated value, it would stop prematurely. Not exactly soon in terms of time, but still, in a state where the array was only half-sorted. This is where [@ngg](https://github.com/ngg) came to the rescue, and designed an algorithm that skips over sorting steps and decrements the counter faster than just running the original algorithm, but still stopping when the original algorithm would have stopped. Plugging in that algorithm produced the key in just a few seconds:
+
+```
+Seed: 0000B089
+Flag: hitcon{nth_perm_Ruby_for_writing_X_C_for_running}
+```
+
